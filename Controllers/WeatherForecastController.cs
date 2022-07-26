4f67e0c27cd1cc374b295b08795c834d6cc2884a
RommelOlachea <msc.rommel.olachea@gmail.com>
2022-07-26 14:48:12 -0600

    se  modifico la ruta del controlador

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -11,22 +11,41 @@ public class WeatherForecastController : ControllerBase
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
+    private static List<WeatherForecast>  ListWeatherForeCast = new List<WeatherForecast>();
     private readonly ILogger<WeatherForecastController> _logger;
 
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
         _logger = logger;
+        if(ListWeatherForeCast == null || !ListWeatherForeCast.Any())
+        {
+            ListWeatherForeCast = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = Random.Shared.Next(-20, 55),            
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToList();
+        }
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),            
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        return ListWeatherForeCast;
+    }
+
+    [HttpPost]
+    public IActionResult Post(WeatherForecast weatherForeCast)
+    {
+        ListWeatherForeCast.Add(weatherForeCast);
+        return Ok();
+    }
+
+    [HttpDelete("{index}")]
+    public IActionResult Delete(int index)
+    {
+        ListWeatherForeCast.RemoveAt(index);
+        return Ok("rommel");
     }
 }
